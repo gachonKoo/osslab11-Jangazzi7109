43a99e46786f81ad3d44b138032ce3a83beb368f
Jangazzi7109 <did2902@gachon.ac.kr>
2025-11-16 20:29:10 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,1 +1,11 @@
-# tester for geo package
+from geo.utils import to_float, circle_area
+
+def main():
+    c = to_float(5)
+    area = circle_area(10)   # π * 10^2 = 314.1592653589793
+
+    print(f"c = {c}")
+    print(f"area = {area}")
+
+if __name__ == "__main__":
+    main()
